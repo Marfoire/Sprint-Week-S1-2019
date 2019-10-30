4dd3d24186afec381edda563834718f16e1a84c1
Alex Durocher <durochal@sheridancollege.ca>
2019-10-30 15:50:12 -0400

    Moving platform

--- Assets/moving.cs
+++ Assets/moving.cs
@@ -6,16 +6,12 @@ public class moving : MonoBehaviour
 {
     public float pathDistance = 3;
     public float moveSpeed = 2;
+    Rigidbody2D Rb2D;
 
     private void FixedUpdate()
     {
-        Rigidbody2D Rb2D = GetComponent<Rigidbody2D>();
+        Rb2D = GetComponent<Rigidbody2D>();
         float newLocation = Mathf.Sin(Time.time * moveSpeed);
         Rb2D.position = new Vector3(newLocation * pathDistance, Rb2D.position.y);
     }
-
-    /*void OnCollisionEnter2D(Collision2D collision)
-    {
-        collision.collider.transform.SetParent(transform);
-    }*/
 }
