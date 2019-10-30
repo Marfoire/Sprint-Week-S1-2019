8c48fef6a67f5b3b9828b68f2161e89ce2a8b41b
lori <48799995+lrsgbh@users.noreply.github.com>
2019-10-30 15:11:25 -0400

    Start Screen

---
+++ Assets/Scripts/ButtonStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonStart : MonoBehaviour
+{
+    public Button btnStart;
+
+    void Start()
+    {
+        Button btn = btnStart.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
+    }
+
+
+    void StartGame() {
+        //Load gameplay scene
+        SceneManager.LoadScene("Bullet Hell Stage");
+    }
+}
