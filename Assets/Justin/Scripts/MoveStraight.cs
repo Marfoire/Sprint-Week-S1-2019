a791c8177ae456d5c70f72ab12e215f276ce151b
Justin Vrieling <justin.vrieling@hotmail.com>
2019-10-30 17:55:53 -0400

    Make a train and launcher prefabs
    Make a train prefab, set to move in one direction at a set velocity.
    Make a train launcher tunnel to periodically launch trains at a speed and distance defined by the launcher.

---
+++ Assets/Justin/Scripts/MoveStraight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStraight : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float maxXOffset;
+
+    private Vector3 initialPosition;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveSpeed, 0, 0);
+
+        if(transform.position.x >= initialPosition.x + maxXOffset)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(initialPosition.x + maxXOffset, 0, 0));
+        Gizmos.color = Color.white;
+    }
+}
---
+++ Assets/Justin/Scripts/TrainLauncher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainLauncher : MonoBehaviour
+{
+    public float launchRate = 5;
+    public float trainSpeed = 10;
+   
+
+    public Transform trainSpawnPoint;
+    public float trainMaxXOffset; 
+
+    public GameObject trainPrefab;
+
+    private float trainTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        if(trainTimer <= 0)
+        {
+            GameObject tempTrain = Instantiate(trainPrefab, trainSpawnPoint.position, Quaternion.Euler(0,90,0));
+            MoveStraight tempMs = tempTrain.GetComponent<MoveStraight>();
+            tempMs.moveSpeed = trainSpeed;
+            tempMs.maxXOffset = trainMaxXOffset;
+
+
+            trainTimer = launchRate;
+        }
+        else
+        {
+            trainTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(255, 140, 0);
+        Gizmos.DrawLine(trainSpawnPoint.position, trainSpawnPoint.position + new Vector3(trainSpawnPoint.position.x + trainMaxXOffset, 0, 0));
+        Gizmos.color = Color.white;
+    }
+}
