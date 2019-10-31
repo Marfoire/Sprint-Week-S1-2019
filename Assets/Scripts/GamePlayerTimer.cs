6ef2882f8594cc98f0815a0415d8b367378723d9
scottD <duff.scott.richard@gmail.com>
2019-10-31 15:19:38 -0400

    timer fades in and out

--- Assets/Scripts/GamePlayerTimer.cs
+++ Assets/Scripts/GamePlayerTimer.cs
@@ -16,14 +16,22 @@ public class GamePlayerTimer : MonoBehaviour
     public Text blueScoreTimer;
 
     public float targetTime;
+    public TextMesh targetTimeMesh;
+    public Bounds bound;
 
     public GameObject gameObject;
 
     private int m_timerBlueScore;
     private int m_timerRedScore;
 
+    private float timeSoFar;
+
     private IEnumerator coroutine;
-    
+    private IEnumerator coroutine2;
+    private void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
@@ -31,12 +39,27 @@ public class GamePlayerTimer : MonoBehaviour
         targetTime -= Time.deltaTime;
         timerText.text = ((int)targetTime).ToString();
 
-        coroutine = disabletimer(5);
-        StartCoroutine(coroutine);
+
+        if(targetTime <= 30 && targetTime >= 25)
+        {
+            coroutine = disabletimer();
+            StartCoroutine(coroutine);
+
+        }
+        
         if (targetTime <= 0.0f)
         {
 
             timerEnded();
+        }
+
+        if(targetTime <= 10)
+        {
+
+            coroutine2 = enabletimer();
+            StartCoroutine(coroutine2);
+
+
         }
 
     }
@@ -59,14 +82,17 @@ public class GamePlayerTimer : MonoBehaviour
         m_timerBlueScore = gameObject.GetComponent<ScoreBoard>().m_blueScore;
         m_timerRedScore = gameObject.GetComponent<ScoreBoard>().m_redScore;
 
-        if( m_timerBlueScore > m_timerRedScore)
+        int m_BlueScoreEndedOn = m_timerBlueScore;
+        int m_RedScoreEndedOn = m_timerRedScore;
+
+        if(m_BlueScoreEndedOn > m_RedScoreEndedOn)
         {
             // BLUE WINSS
             blueWinsText.enabled = true;
 
         }
 
-        if (m_timerRedScore > m_timerBlueScore)
+        if (m_RedScoreEndedOn > m_BlueScoreEndedOn)
         {
 
             // RED WINS
@@ -74,10 +100,27 @@ public class GamePlayerTimer : MonoBehaviour
 
         }
     }
-    private IEnumerator disabletimer(float waitTime)
+    private IEnumerator disabletimer()
     {
-        yield return new WaitForSeconds(waitTime);
-        timerText.enabled = false;
+        timerText.color = new Color(timerText.color.r, timerText.color.g, timerText.color.b, 1);
+         while(timerText.color.a > 0.5f)
+        {
+            timerText.color = new Color(timerText.color.r, timerText.color.g, timerText.color.b, timerText.color.a - (Time.deltaTime / 5));
+            yield return null;
+        }
+        
+    }
+
+
+    private IEnumerator enabletimer()
+    {
+        timerText.color = new Color(timerText.color.r, timerText.color.g, timerText.color.b, ((float)0.5));
+        while (timerText.color.a < 1f)
+        {
+            timerText.color = new Color(timerText.color.r, timerText.color.g, timerText.color.b, timerText.color.a + (Time.deltaTime / 5));
+            yield return null;
+        }
+
 
     }
 
