2adca92242b9c71d5bbc5601794032b65bb8d5cf
Justin Vrieling <justin.vrieling@hotmail.com>
2019-10-31 13:24:21 -0400

    Add UI Elements
    added an intro countdown and score counter.
    Call UIManager.uiManager.SetScre(float score) to update the score.
    
    Prefabbed the UI

---
+++ Assets/Justin/Scripts/UIScripts/StartingFlash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingFlash : MonoBehaviour
+{
+    public GameObject readyUI;
+    public Text counter;
+    public GameObject ready;
+    public GameObject go;
+
+    [Header("SendMessage to chosen object")]
+    public GameObject recipient;
+    public string message;
+
+    public IEnumerator StartGame()
+    {
+        readyUI.SetActive(true);
+        go.SetActive(false);
+        ready.SetActive(true);
+
+        counter.text = "3";
+
+        yield return new WaitForSeconds(1);
+
+        counter.text = "2";
+
+        yield return new WaitForSeconds(1);
+        counter.text = "1";
+
+        yield return new WaitForSeconds(1);
+        go.SetActive(true);
+        ready.SetActive(false);
+        counter.text = "";
+
+        if (recipient)
+        {
+            recipient.SendMessage(message);
+        }
+    }
+}
